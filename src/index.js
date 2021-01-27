8f793488558f9c0969b093963fe964f5ba614fcf
alexius90e <alexius90e@mail.ru>
2021-01-27 22:59:14 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
+    
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce(function (sum, item) {return sum + item}, 0) / array.length;
+    }
+};
